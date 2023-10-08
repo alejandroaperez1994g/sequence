1d7d7a7d00317609b51dbe4ff3bbd9c965bcc65c
Alejandro <alejandroaperez1994g@gmail.com>
2023-10-08 13:22:08 +0200

    feat(tests): set up jest and configure testing environment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,16 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "jest": true,
     },
-    "extends": ["standard-with-typescript","plugin:@typescript-eslint/recommended-type-checked"],
+    "extends": ["standard-with-typescript",
+        "plugin:@typescript-eslint/recommended-type-checked","plugin:jest/recommended"],
     "overrides": [
         {
             "env": {
-                "node": true
+                "node": true,
+                "jest": true,
             },
             "files": [
                 ".eslintrc.{js,cjs}"
@@ -17,10 +20,16 @@ module.exports = {
     "parserOptions": {
         "project": true,
         "tsconfigRootDir": __dirname,
+        "sourceType": "module"
     },
-
+    //"plugins": ["jest"],
     "rules": {
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/no-misused-promises": "off",
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error"
     }
 }
